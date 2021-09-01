485a36855ca17c1768cc6043a9f9e33fb0f0fd7d
Brbuck <62665590+Brbuck@users.noreply.github.com>
2021-09-01 14:44:52 -0300

    Update

---
+++ src/components/HeaderDropDown/DataPrincipal.js
@@ -0,0 +1,50 @@
+export const DataPrincipal = [
+    {
+        image: 'https://catalogo.webmotors.com.br/imagens/prod/portal/img/logos/carro/audi.png/?w=64&h=64',
+        name: 'Audi'
+    },
+    {
+        image: 'https://catalogo.webmotors.com.br/imagens/prod/portal/img/logos/carro/bmw.png/?w=64&h=64',
+        name: 'BMW'
+    },
+    {
+        image: 'https://catalogo.webmotors.com.br/imagens/prod/portal/img/logos/carro/chevrolet.png/?w=64&h=64',
+        name: 'Chevrolet'
+    },
+    {
+        image: 'https://catalogo.webmotors.com.br/imagens/prod/portal/img/logos/carro/ferrari.png/?w=64&h=64',
+        name: 'Ferrari'
+    },
+    {
+        image: 'https://catalogo.webmotors.com.br/imagens/prod/portal/img/logos/carro/honda.png/?w=64&h=64',
+        name: 'Honda'
+    },
+    {
+        image: 'https://catalogo.webmotors.com.br/imagens/prod/portal/img/logos/carro/jeep.png/?w=64&h=64',
+        name: 'Jeep'
+    },
+    {
+        image: 'https://catalogo.webmotors.com.br/imagens/prod/portal/img/logos/carro/lamborghini.png/?w=64&h=64',
+        name: 'Lamborghini'
+    },
+    {
+        image: 'https://catalogo.webmotors.com.br/imagens/prod/portal/img/logos/carro/mclaren.png/?w=64&h=64',
+        name: 'Mclaren'
+    },
+    {
+        image: 'https://catalogo.webmotors.com.br/imagens/prod/portal/img/logos/carro/nissan.png/?w=64&h=64',
+        name: 'Nissan'
+    },
+    {
+        image: 'https://catalogo.webmotors.com.br/imagens/prod/portal/img/logos/carro/porsche.png/?w=64&h=64',
+        name: 'Porsche'
+    },
+    {
+        image: 'https://catalogo.webmotors.com.br/imagens/prod/portal/img/logos/carro/toyota.png/?w=64&h=64',
+        name: 'Toyota'
+    },
+    {
+        image: 'https://catalogo.webmotors.com.br/imagens/prod/portal/img/logos/carro/volkswagen.png/?w=64&h=64',
+        name: 'Volkswagen'
+    }
+]
